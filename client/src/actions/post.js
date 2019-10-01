b26d5d405960fbeb1998a906970610c761bb480f
Alvaro De La Torre <alvarodltp@gmail.com>
2019-10-01 15:03:41 -0400

    add comment form and single comment component

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
-  GET_POST
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from './types';
 
 // Get posts
@@ -121,50 +123,50 @@ export const getPost = id => async dispatch => {
   }
 };
 
-// // Add comment
-// export const addComment = (postId, formData) => async dispatch => {
-//   const config = {
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   };
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
 
-//   try {
-//     const res = await axios.post(
-//       `/api/posts/comment/${postId}`,
-//       formData,
-//       config
-//     );
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    );
 
-//     dispatch({
-//       type: ADD_COMMENT,
-//       payload: res.data
-//     });
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data
+    });
 
-//     dispatch(setAlert('Comment Added', 'success'));
-//   } catch (err) {
-//     dispatch({
-//       type: POST_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status }
-//     });
-//   }
-// };
+    dispatch(setAlert('Comment Added', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
 
-// // Delete comment
-// export const deleteComment = (postId, commentId) => async dispatch => {
-//   try {
-//     await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+// Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
 
-//     dispatch({
-//       type: REMOVE_COMMENT,
-//       payload: commentId
-//     });
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId
+    });
 
-//     dispatch(setAlert('Comment Removed', 'success'));
-//   } catch (err) {
-//     dispatch({
-//       type: POST_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status }
-//     });
-//   }
-// };
+    dispatch(setAlert('Comment Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layouts/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -17,6 +19,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       Back To Posts
     </Link>
     <PostItem post={post} showActions={false} />
+    <CommentForm postId={post._id} />
+    <div class="comments">
+      {post.comments.map(comment => (
+        <CommentItem key={comment._id} comment={comment} postId={post._id} />
+      ))}
+    </div>
   </React.Fragment>
 }
 
