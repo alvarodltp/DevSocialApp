9a6bee9abf5c6a4c6c4010f0d582823a294cabe1
Alvaro De La Torre <alvarodltp@gmail.com>
2019-10-01 11:06:33 -0400

    add and remove like funcionality

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,7 +5,10 @@ import { connect } from 'react-redux';
 import Spinner from '../layouts/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
-import ProfileAbout from './ProfileAbout'
+import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 
 const Profile = ({ getProfileById, profile: {profile, loading}, auth, match }) => {
@@ -24,6 +27,33 @@ const Profile = ({ getProfileById, profile: {profile, loading}, auth, match }) =
         <div class="profile-grid my-1">
           <ProfileTop profile={profile} />
           <ProfileAbout profile={profile} />
+          <div className="profile-exp bg-white p-2">
+            <h2 className="text-primary">
+              Experience
+            </h2>
+            {profile.experience.length > 0 ? (
+              <React.Fragment>
+                {profile.experiece.map(experience => (
+                  <ProfileExperience key={experience._id} experience={experience}/>
+                ))}
+              </React.Fragment>
+            ) : <h4>No experience credentials</h4>}
+          </div>
+          <div className="profile-edu bg-white p-2">
+            <h2 className="text-primary">
+              Education
+            </h2>
+            {profile.education.length > 0 ? (
+              <React.Fragment>
+                {profile.education.map(education => (
+                  <ProfileEducation key={education._id} education={education}/>
+                ))}
+              </React.Fragment>
+            ) : <h4>No education credentials</h4>}
+          </div>
+          {profile.githubusername && (
+            <ProfileGithub username={profile.githubusername}/>
+          )}
         </div>
       </React.Fragment>}
     </React.Fragment>
